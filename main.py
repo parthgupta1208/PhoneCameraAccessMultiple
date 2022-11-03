535eee28865cd105c8e075dfdf866892bcfb8f9a
Parth Gupta <parthgupta3003@gmail.com>
2022-11-04 00:09:49 +0530

    without GUI

--- main.py
+++ main.py
@@ -2,37 +2,32 @@ import cv2
 import numpy as np
 import threading
 
-# url = "http://192.168.184.246:8080/video"
-# cap = cv2.VideoCapture(url)
-# while(True):
-#     camera, frame = cap.read()
-#     if frame is not None:
-#         cv2.imshow("Frame", frame)
-#     q = cv2.waitKey(1)
-#     if q==ord("q"):
-#         break
-# cv2.destroyAllWindows()
-
 class camThread(threading.Thread):
-    def __init__(self, camID):
+    def __init__(self, previewName, camID):
         threading.Thread.__init__(self)
+        self.previewName = previewName
         self.camID = camID
     def run(self):
-        camPreview(self.camID)
+        print ("Starting " + self.previewName)
+        camPreview(self.previewName, self.camID)
 
-def camPreview(camID):
-    cap = cv2.VideoCapture(camID)
-    while(True):
-        camera, frame = cap.read()
-        if frame is not None:
-            cv2.imshow("camID", frame)
-        q = cv2.waitKey(1)
-        if q==ord("q"):
+def camPreview(previewName, camID):
+    cv2.namedWindow(previewName)
+    cam = cv2.VideoCapture(camID)
+    if cam.isOpened():
+        rval, frame = cam.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow(previewName, frame)
+        rval, frame = cam.read()
+        key = cv2.waitKey(20)
+        if key == 27:
             break
-    cv2.destroyAllWindows()
+    cv2.destroyWindow(previewName)
 
-# Create two threads as follows
-thread1 = camThread("http://192.168.184.202:8080/video")
-thread2 = camThread("http://192.168.184.246:8080/video")
+thread1 = camThread("Cam1","http://192.168.184.202:8080/video")
+thread2 = camThread("Cam2","http://192.168.184.246:8080/video")
 thread1.start()
 thread2.start()
